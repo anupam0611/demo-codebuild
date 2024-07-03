7d867d9df74b5a3474dcd2db10ac0b7fc12e91e5
anupam0611 <anupamaggarwal.0611@gmail.com>
2024-07-03 12:23:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -x
-sudo apt install openjdk-17-jdk openjdk-17-jre >> /var/tmp/app-deploy.log 2>&1
+sudo apt install -y openjdk-17-jdk openjdk-17-jre >> /var/tmp/app-deploy.log 2>&1
