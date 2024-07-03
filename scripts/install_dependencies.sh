4fbb4565685c8e4014794bb0ecd9c0660bfece59
anupam0611 <anupamaggarwal.0611@gmail.com>
2024-07-03 12:23:15 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 set -x
-sudo yum -y remove java-1.6* java-1.7* > /var/tmp/app-deploy.log 2>&1
-sudo yum -y install java-1.8* >> /var/tmp/app-deploy.log 2>&1
+sudo apt install openjdk-17-jdk openjdk-17-jre >> /var/tmp/app-deploy.log 2>&1
